4a86cd92e6c67a9db7d482620f2dc71b32153ab1
Katie <adlerk8@gmail.com>
2020-11-05 15:37:58 -0700

    done through step 8

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class About extends Component {
   render() {
     return (
       <div>
         <div className='subnav'>
+          <Link className='subnav_links'>
+            <h3>About</h3>
+          </Link>
+          <Link className='subnav_links'>
+            <h3>History</h3>
+          </Link>
+          <Link className='subnav_links'>
+            <h3>Contact</h3>
+          </Link>
         </div>
         <div className='box'>
         </div>
